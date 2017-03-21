f8c9cc5042c951ed402b052d04738c14589d1b00
RuudyLee <rudy.lee.318@gmail.com>
2017-03-21 10:32:05 -0400

    Added answer buttons and some functionality. Changed names of some files

--- Assets/Scripts/RandomSound.cs
+++ Assets/Scripts/RandomSound.cs
@@ -17,41 +17,77 @@ public class RandomSound : MonoBehaviour
 
     List<Pair<string, AudioClip>> heartSounds;
     List<Pair<string, AudioClip>> lungSounds;
+    List<Pair<string, AudioClip>> allSounds;
+
+    Button guessButton;
+    public Button[] answerButtons;
+    string correctAnswer;
+    int correctAnswerPosition;
 
     // Use this for initialization
     void Start()
     {
         heartSounds = new List<Pair<string, AudioClip>>();
         lungSounds = new List<Pair<string, AudioClip>>();
+        allSounds = new List<Pair<string, AudioClip>>();
 
         // Load all sounds
         StartCoroutine(LoadSounds());
-        
+
         audioSource = GetComponent<AudioSource>();
+
+        guessButton = GameObject.Find("Guess").GetComponent<Button>();
+        guessButton.onClick.AddListener(guessOnClick);
     }
 
     public void ChooseRandomSound()
     {
+        // Randomly select a sound to play
         int heartOrLung = Random.Range(0, 2);
+        int random;
         if (heartOrLung == 0) // heart
         {
-            int random = Random.Range(0, heartSounds.Count);
+            random = Random.Range(0, heartSounds.Count);
             audioSource.clip = heartSounds[random].Second;
             transform.position = heartLocation.position;
             audioSource.Play();
+            correctAnswer = heartSounds[random].First;
             debugText.text = "Now Playing:\n" + heartSounds[random].First;
         }
         else if (heartOrLung == 1) // lung
         {
-            int random = Random.Range(0, lungSounds.Count);
+            random = Random.Range(0, lungSounds.Count);
             audioSource.clip = lungSounds[random].Second;
             transform.position = lungLocation.position;
             audioSource.Play();
+            correctAnswer = lungSounds[random].First;
             debugText.text = "Now Playing:\n" + lungSounds[random].First;
         }
         else
         {
             Debug.Log("You should never see this");
+        }
+
+        // Update answer buttons
+        correctAnswerPosition = Random.Range(0, 4);
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            if (i == correctAnswerPosition)
+            {
+                // place correct answer here
+                answerButtons[i].GetComponentInChildren<Text>().text = correctAnswer;
+            }
+            else
+            {
+                // Ensure the incorrect choices don't have the same label
+                int randomWords;
+                do
+                {
+                    randomWords = Random.Range(0, allSounds.Count);
+                } while (randomWords == correctAnswerPosition);
+
+                answerButtons[i].GetComponentInChildren<Text>().text = allSounds[randomWords].First;
+            }
         }
     }
 
@@ -60,7 +96,6 @@ public class RandomSound : MonoBehaviour
         // Load heart sounds
         DirectoryInfo dir = new DirectoryInfo("Assets/Sounds/Heart");
         FileInfo[] info = dir.GetFiles("*.wav");
-
 
         foreach (FileInfo fi in info)
         {
@@ -74,10 +109,8 @@ public class RandomSound : MonoBehaviour
         }
 
         // load lung sounds     
-
         dir = new DirectoryInfo("Assets/Sounds/Lungs");
         info = dir.GetFiles("*.wav");
-
 
         foreach (FileInfo fi in info)
         {
@@ -90,6 +123,11 @@ public class RandomSound : MonoBehaviour
             lungSounds.Add(new Pair<string, AudioClip>(fi.Name.Substring(0, fi.Name.Length - 4), ac));
         }
 
+        // store a concatenated list
+        allSounds.AddRange(heartSounds);
+        allSounds.AddRange(lungSounds);
+
+        // boot it up!
         ChooseRandomSound();
     }
 
@@ -98,6 +136,15 @@ public class RandomSound : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             ChooseRandomSound();
+        }
+    }
+
+    void guessOnClick()
+    {
+        // Toggle buttons
+        foreach (Button b in answerButtons)
+        {
+            b.gameObject.SetActive(!b.gameObject.activeInHierarchy);
         }
     }
 }
